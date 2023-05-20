96d6a7b9524727d58442e26381e9e12388311b6d
KiyoCat <romyzie00@gmail.com>
2023-05-20 17:18:50 +0200

    commit all

--- app/src/main/java/de/haw_hamburg/smartclassroom/MainActivity.java
+++ app/src/main/java/de/haw_hamburg/smartclassroom/MainActivity.java
@@ -1,14 +1,20 @@
 package de.haw_hamburg.smartclassroom;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "TAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setAdapter(new CustomAdapter());
     }
 }
