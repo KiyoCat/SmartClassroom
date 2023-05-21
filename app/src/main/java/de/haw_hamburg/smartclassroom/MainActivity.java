d6fd19810c56c8bf85245da6b1c19e67aac228b1
KiyoCat <romyzie00@gmail.com>
2023-05-21 12:55:10 +0200

    commit all

--- app/src/main/java/de/haw_hamburg/smartclassroom/MainActivity.java
+++ app/src/main/java/de/haw_hamburg/smartclassroom/MainActivity.java
@@ -4,17 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "TAG";
+    Button button;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setAdapter(new CustomAdapter());
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openRoomActivity();
+            }
+        });
+
+        imageView = (ImageView) findViewById(R.id.imageView3);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewRoomActivity();
+            }
+        });
+
+    }
+    public void openRoomActivity(){
+        Intent intent = new Intent(this, RoomActivity.class);
+        startActivity(intent);
+    }
+    public void openNewRoomActivity(){
+        Intent intent = new Intent(this, NewRoomActivity.class);
+        startActivity(intent);
     }
 }
