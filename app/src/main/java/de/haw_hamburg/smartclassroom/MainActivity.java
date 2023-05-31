18fdf666b7cad0aa79f58719ac0c4c26656e3e1b
sphllg <sophie.marie.hellige@gmail.com>
2023-05-31 13:30:01 +0200

    mqtt implementation

--- app/src/main/java/de/haw_hamburg/smartclassroom/MainActivity.java
+++ app/src/main/java/de/haw_hamburg/smartclassroom/MainActivity.java
@@ -11,17 +11,24 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
     ImageView imageView;
+    private static final String BROKER_URL = "tcp://broker.hivemq.com:1883";
+    private static final String CLIENT_ID = "my-mqtt-client-id";
+    private MqttHandler mqttHandler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         recyclerView.setAdapter(new CustomAdapter());
+
+        mqttHandler = new MqttHandler();
+        mqttHandler.connect(BROKER_URL, CLIENT_ID);
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener(){
@@ -48,4 +55,19 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, NewRoomActivity.class);
         startActivity(intent);
     }
+    public void publishMessage(String topic, String message){
+        Toast.makeText(this, "Publishing message: " + message,Toast.LENGTH_SHORT).show();
+        mqttHandler.publish(topic, message);
+    }
+    private void subscribeToTopic(String topic){
+        Toast.makeText(this, "Subscribing to topic: " + topic, Toast.LENGTH_SHORT).show();
+        mqttHandler.subscribe(topic);
+    }
+    @Override
+    protected void onDestroy() {
+        mqttHandler.disconnect();
+        super.onDestroy();
+    }
+
+
 }
