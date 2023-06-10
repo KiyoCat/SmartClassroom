aa20ef8f9be4885aa499f0a3a717f2d09af1ec4d
Murphyuwu <remnantscs@gmail.com>
2023-06-10 14:51:48 +0200

    configured seekbar

--- app/src/main/java/de/haw_hamburg/smartclassroom/RoomActivity.java
+++ app/src/main/java/de/haw_hamburg/smartclassroom/RoomActivity.java
@@ -4,16 +4,42 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.w3c.dom.Text;
+
 public class RoomActivity extends AppCompatActivity {
     ImageView imageView;
+    SeekBar seekBar;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.room_activity);
+
+        seekBar = (SeekBar) findViewById(R.id.heating_seekbar);
+        textView = (TextView) findViewById(R.id.heating_textview);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textView.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
 
         imageView = (ImageView) findViewById(R.id.imageView9);
         imageView.setOnClickListener(new View.OnClickListener() {
