07bc0e8a2880b7e44fa9e199a11795947504ef23
sphllg <sophie.marie.hellige@gmail.com>
2023-06-18 10:27:40 +0200

    restore master

--- app/src/main/java/de/haw_hamburg/smartclassroom/View/RoomActivity.java
+++ app/src/main/java/de/haw_hamburg/smartclassroom/View/RoomActivity.java
@@ -9,12 +9,10 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
-import androidx.lifecycle.ViewModelProvider;
 
 import de.haw_hamburg.smartclassroom.R;
-import de.haw_hamburg.smartclassroom.viewmodel.SmartClassroomController;
 import de.haw_hamburg.smartclassroom.databinding.RoomActivityBinding;
+import de.haw_hamburg.smartclassroom.viewmodel.SmartClassroomController;
 
 public class RoomActivity extends AppCompatActivity {
     private RoomActivityBinding binding;
@@ -28,7 +26,7 @@ public class RoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.room_activity);
-
+/*
         binding = DataBindingUtil.setContentView(this, R.layout.room_activity);
         viewModel = new ViewModelProvider(this).get(SmartClassroomController.class);
         binding.setSmartClassroomController(viewModel);
@@ -37,7 +35,7 @@ public class RoomActivity extends AppCompatActivity {
 
         rollosSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
             viewModel.rollosSwitchisClicked(isChecked);
-        });
+        });*/
 
         seekBar = (SeekBar) findViewById(R.id.heating_seekbar);
         textView = (TextView) findViewById(R.id.heating_textview);
