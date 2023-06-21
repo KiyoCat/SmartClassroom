c53b4d3e79f9e233bb0b7becd36b60c3e7588c34
sphllg <sophie.marie.hellige@gmail.com>
2023-06-21 12:09:48 +0200

    remove notifications from roomactivity

--- app/src/main/java/de/haw_hamburg/smartclassroom/View/RoomActivity.java
+++ app/src/main/java/de/haw_hamburg/smartclassroom/View/RoomActivity.java
@@ -1,12 +1,7 @@
 package de.haw_hamburg.smartclassroom.View;
 
-import android.Manifest;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.SeekBar;
@@ -14,10 +9,6 @@ import android.widget.Switch;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
-import androidx.core.content.ContextCompat;
 import androidx.lifecycle.ViewModelProvider;
 
 import de.haw_hamburg.smartclassroom.Model.MqttClient;
@@ -84,40 +75,18 @@ public class RoomActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-
+/*
         if (ContextCompat.checkSelfPermission(RoomActivity.this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
             Log.d("error", "permission for notifications not granted");
             ActivityCompat.requestPermissions(RoomActivity.this, new String[]{Manifest.permission.POST_NOTIFICATIONS}, 1);
         } else {
             showNotification(receivedMessage);
-        }
+        }*/
     }
 
     public void openMainActivity(){
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
-    }
-    private void showNotification(String receivedMessage) {
-        int convertTemperatureToInt = Integer.parseInt(receivedMessage);
-        if (convertTemperatureToInt > 20) {
-            NotificationCompat.Builder builder = new NotificationCompat.Builder(RoomActivity.this, "notif");
-            builder.setContentTitle("Smart Classroom");
-            builder.setContentText("It's warm outside, turn down the heating!");
-            builder.setSmallIcon(R.drawable.notif_bell);
-            builder.setAutoCancel(true);
-            builder.setPriority(NotificationCompat.PRIORITY_HIGH);
-
-            NotificationChannel channel = null;
-            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-                channel = new NotificationChannel("notif", "Notifications", NotificationManager.IMPORTANCE_HIGH);
-                channel.setDescription("General Notifications");
-                channel.enableLights(true);
-                NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            }
-
-            NotificationManagerCompat managerCompat = NotificationManagerCompat.from(RoomActivity.this);
-            managerCompat.notify(123, builder.build());
-        }
     }
 
 }
