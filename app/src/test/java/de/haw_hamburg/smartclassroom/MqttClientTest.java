c33c31ec77af57a5e4779a52a8ba87d239cb8118
sphllg <sophie.marie.hellige@gmail.com>
2023-06-25 21:11:41 +0200

    final commit

--- app/src/test/java/de/haw_hamburg/smartclassroom/MqttClientTest.java
+++ app/src/test/java/de/haw_hamburg/smartclassroom/MqttClientTest.java
@@ -1,6 +1,6 @@
 package de.haw_hamburg.smartclassroom;
 
-/*import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.doThrow;
@@ -224,4 +224,4 @@ public class MqttClientTest implements IMqttMessageListener {
         // no logic
     }
 
-}*/
+}
